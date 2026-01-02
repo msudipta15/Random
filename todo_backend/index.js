813c6a0d353e0d65e5c211bc5162663f5651a391
Sudipta <techtv70@gmail.com>
2026-01-02 13:10:20 +0530

    .

---
+++ todo_backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+async function main() {
+  try {
+    if (!process.env.db_url) {
+      console.log("Invalid db connection string !");
+      return;
+    }
+    await mongoose.connect(process.env.db_url).then(() => {
+      console.log("Connected to db.");
+    });
+  } catch (error) {
+    console.log("Error connecting to db !");
+  }
+}
+
+main();
