649a17311f777804c709760bf9f478ccbb47555c
Sudipta <techtv70@gmail.com>
2026-01-03 13:17:58 +0530

    .

--- todo_backend/db.js
+++ todo_backend/db.js
@@ -3,13 +3,20 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
+  username: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true },
 });
 
-const taskSchema = new Schema({
-  userid: { type: ObjectId, ref: user },
-  title: String,
-  completed: { type: Boolean, default: false },
-  time: { type: Date, default: Date.now },
-});
+const taskSchema = new Schema(
+  {
+    userid: { type: ObjectId, ref: user },
+    title: String,
+    completed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("user", userSchema);
+const taskModel = mongoose.model("task", taskSchema);
+
+export { userModel, taskModel };
